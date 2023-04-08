1ab56b7844a42bdda39fdea34d200ec321858e63
jasmit21 <jasmitrathod21@gmail.com>
2023-04-09 05:13:50 +0530

    pi-hole api to add/del websites from blocklist

--- controllers/blockController.js
+++ controllers/blockController.js
@@ -1,27 +1,122 @@
 const pool = require("../models/dbConfig");
-// const session = require("express-session");
-const path = require("path");
-// const bcrypt = require('bcrypt');
+const http = require("http");
+const querystring = require("querystring");
+
 module.exports = {
   post: (req, res) => {
-    let user_phone = req.body.user_phone;
+    let website = req.body.website_url;
     let websiteBlockStatus = req.body.webstatus;
-    qry = `UPDATE WebsiteAndUser_Mapping SET isDisabled = ${websiteBlockStatus} where user_id= (SELECT user_id from user_details where phone_no = ${user_phone})
-    `;
-    pool.query(qry,(err,result)=>{
-        if (err) {
-            throw err
-            res.status(400).json({
-                success:false,
-                msg:"Qry failed to execute"
-            })
-        };
-        console.log("Flag did change");
-        res.status(200).json({
-            success:true,
-            msg:"status updated successfully"
-        })
-    })
+    let qry = `UPDATE website_master SET isDisabled = ${websiteBlockStatus} WHERE website_id= ${website}`;
 
-  }
+    console.log(qry);
+
+    pool.query(qry, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(400).json({
+          success: false,
+          msg: "Qry failed to execute",
+        });
+        return;
+      }
+
+      console.log("Flag did change");
+
+      let qry2 = `SELECT isDisabled FROM website_master WHERE website_id = ${website}`;
+
+      pool.query(qry2, (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(400).json({
+            success: false,
+            msg: "Qry failed to execute",
+          });
+          return;
+        }
+
+        console.log("Ho raha hai!", result[0].isDisabled);
+
+        let PiDisable = result[0].isDisabled;
+
+        if (PiDisable === 1) {
+          console.log("Adding website to blocklist");
+
+          const apiToken = process.env.PI_HOLE_API_KEY;
+          const url = website;
+
+          const postData = querystring.stringify({
+            auth: apiToken,
+            list: "black",
+            add: url,
+          });
+
+          const options = {
+            hostname: process.env.PI_HOST_IP,
+            port: 80,
+            path: "/admin/api.php",
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              "Content-Length": Buffer.byteLength(postData),
+            },
+          };
+
+          const request = http.request(options, (response) => {
+            response.setEncoding("utf8");
+            response.on("data", (chunk) => {
+              console.log(chunk);
+            });
+          });
+
+          request.on("error", (error) => {
+            console.error(error);
+          });
+
+          request.write(postData);
+          request.end();
+        } else {
+          console.log("Removing website from blocklist");
+
+          const apiToken = process.env.PI_HOLE_API_KEY;
+          const url = website;
+
+          const postData = querystring.stringify({
+            auth: apiToken,
+            list: "black",
+            sub: url,
+          });
+
+          const options = {
+            hostname: process.env.PI_HOST_IP,
+            port: 80,
+            path: "/admin/api.php",
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              "Content-Length": Buffer.byteLength(postData),
+            },
+          };
+
+          const request = http.request(options, (response) => {
+            response.setEncoding("utf8");
+            response.on("data", (chunk) => {
+              console.log(chunk);
+            });
+          });
+
+          request.on("error", (error) => {
+            console.error(error);
+          });
+
+          request.write(postData);
+          request.end();
+        }
+
+        res.status(200).json({
+          success: true,
+          msg: "status updated successfully",
+        });
+      });
+    });
+  },
 };
